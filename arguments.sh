a4228c6496bccb4eb168fdc4dd08d263ce83689a
José Rodrigues <jyrodrigues@gmail.com>
2017-09-30 19:37:36 -0300

    Improve arguments.sh with comments and examples

--- arguments.sh
+++ arguments.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 num_arguments=$#
-echo "${num_arguments} arguments were passed"
+echo -e "${num_arguments} arguments were passed\n"
 
-echo "accessing argument number 2: ->${2}<- and next echo calls 3"
-echo $3
+echo "\$# stands for the number of arguments passed"
+echo "\$@ stands for a list of arguments"
+
+echo -e "\n((for i = 1; i <= \$#; i++ )); do echo \"arg #\$i: \${!i}\" done"
+for (( i = 1; i <= $#; i++ )); do
+  echo "arg #$i: ${!i}"
+done
+
+echo -e "\nfor arg in \"\$@\"; do echo \"\$arg\" done"
+for argument in "$@"; do
+  echo "$argument"
+done
+
+echo -e "\nfor arg; do echo \"\$arg\" done"
+for argument; do
+  echo "$argument"
+done
